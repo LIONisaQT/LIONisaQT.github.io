75c9996e4a135f45cef3380275580984f0cf17fb
Ryan <ryan.shee@gmail.com>
2018-03-01 12:05:57 -0800

    Toggle finally works properly

--- toggle.js
+++ toggle.js
@@ -1,7 +1,46 @@
-var lightMode = true;
+let lightMode;
+if (localStorage.getItem('isLight') === null) {
+    console.log('isLight does not exist');
+    localStorage.setItem('isLight', 0)
+} else {
+    console.log('isLight exists');
+}
+lightMode = localStorage.getItem('isLight');
+console.log('lightMode set to ' + lightMode);
+
+window.onload = function() {
+    if (lightMode == 1) {
+        console.log('because lightMode is ' + lightMode + ', setting up page with light theme');
+        document.body.style.backgroundColor="#eee";
+        document.getElementById('nameTitle').style.color="#333";
+        document.getElementById('header').style.backgroundColor="#333";
+        document.getElementById('header').classList.add('dark-header');
+        document.getElementById('wrapper').style.color="#333";
+        document.getElementById('wrapper').style.backgroundColor="#eee";
+        let notes = document.getElementsByClassName('projectNote');
+        for (let i = 0; i < notes.length; i++) {
+          notes[i].style.color="#535353";
+        }
+    } else if (lightMode == 0) {
+        console.log('because lightMode is ' + lightMode + ', setting up page with dark theme');
+        document.body.style.backgroundColor="#333";
+        document.getElementById('nameTitle').style.color="#eee";
+        document.getElementById('header').style.backgroundColor="#222";
+        document.getElementById('header').classList.add('darker-header');
+        document.getElementById('wrapper').style.color="#eee";
+        document.getElementById('wrapper').style.backgroundColor="#333";
+        let notes = document.getElementsByClassName('projectNote');
+        for (let i = 0; i < notes.length; i++) {
+          notes[i].style.color="#bbb";
+        }
+    } else {
+        console.log('lightMode is ' + lightMode + ', you shouldn\'t be here wtf');
+    }
+}
 
 function toggleMode() {
-    if (lightMode) {
+    if (lightMode == 1) {
+        console.log('switching to dark theme');
         document.body.style.backgroundColor="#333";
         document.getElementById('nameTitle').style.color="#eee";
         // document.getElementById('header').style.backgroundColor="#222";
@@ -13,8 +52,10 @@ function toggleMode() {
         for (let i = 0; i < notes.length; i++) {
           notes[i].style.color="#bbb";
         }
-        lightMode = false;
-    } else {
+        lightMode = 0;
+        localStorage.setItem('isLight', lightMode);
+    } else if (lightMode == 0) {
+        console.log('switching to light theme');
         document.body.style.backgroundColor="#eee";
         document.getElementById('nameTitle').style.color="#333";
         document.getElementById('header').style.backgroundColor="#333";
@@ -26,6 +67,7 @@ function toggleMode() {
         for (let i = 0; i < notes.length; i++) {
           notes[i].style.color="#535353";
         }
-        lightMode = true;
+        lightMode = 1;
+        localStorage.setItem('isLight', lightMode);
     }
 }
