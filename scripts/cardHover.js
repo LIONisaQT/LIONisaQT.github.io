aeed379b71a6342865231e74a53989a214230187
Ryan <ryan.shee@gmail.com>
2018-04-24 03:57:37 -0700

    Finished hover, added more color pallates

--- scripts/cardHover.js
+++ scripts/cardHover.js
@@ -1,12 +1,50 @@
-function getCoords(event, target) {
+// Tilts cards based on mouse position
+/*
+  Tried to make tilt scale off distance from center, but tilt
+  would stutter/was not smooth, so I ditched it for now.
+*/
+function tiltCard(event, target) {
+  // let multiplier = 4;
+
+  // Get mouse position
   let mouseX = event.clientX;
   let mouseY = event.clientY;
-  let centerX = target.offsetLeft + target.offsetWidth / 2;
-  let centerY = target.offsetTop + target.offsetHeight / 2;
-  console.log(target.style.transform = "perspective(300px) rotateX(3deg) rotateY(3deg)");
-  // if (mouseX > centerX) {
-  //   target.
-  // }
-  // console.log(centerX + ', ' + centerY);
-  // console.log(ele.id + ': ' +x + ', ' + y);
+
+  // Get hitbox of card basically
+  let position = target.getBoundingClientRect();
+
+  // Center of card
+  let centerX = position.left + position.width / 2;
+  let centerY = position.top + position.height / 2;
+
+  // Set rotations
+  let rotX, rotY;
+
+  if (mouseX > centerX) {
+    rotY = -3;
+    // rotY = -mouseX/centerX * multiplier;
+  } else if (mouseX < centerX) {
+    rotY = 3;
+    // rotY = mouseX/centerX * multiplier;
+  } else {
+    rotY = 0;
+  } 
+
+  if (mouseY > centerY) {
+    rotX = 3;
+    // rotX = mouseY/centerY * multiplier;
+  } else if (mouseY < centerY) {
+    rotX = -3;
+    // rotX = -mouseY/centerY * multiplier;
+  } else {
+    rotX = 0;
+  }
+
+  // Change transform
+  target.style.transform = "perspective(300px) rotateX(" + rotX + "deg) rotateY(" + rotY + "deg)";
+}
+
+// Resets card's transform
+function resetCard(target) {
+  target.style.transform = "";
 }
