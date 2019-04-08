342afbc8d45e72058d43794a6bcf37fbe6c0d479
Ryan Shee <ryan.shee@gmail.com>
2019-04-07 19:48:14 -0700

    April 2019 update

--- scripts/aboutSlideshow.js
+++ scripts/aboutSlideshow.js
@@ -1,9 +1,7 @@
 // https://stackoverflow.com/questions/25347946/add-fade-effect-in-slideshow-javascript
 
 var imgArray = [
-	'/static/img/boo/boardwalk.jpg',
-	'/static/img/boo/great-america.jpg',
-	'/static/img/boo/tea-garden.jpg'],
+	'/static/img/portrait.JPG',],
 	curIndex = 0;
 	imgDuration = 3000;
 
@@ -17,4 +15,4 @@ function slideShow() {
 	if (curIndex == imgArray.length) { curIndex = 0; }
 	setTimeout(slideShow, imgDuration);
 }
-slideShow();
+// slideShow();
