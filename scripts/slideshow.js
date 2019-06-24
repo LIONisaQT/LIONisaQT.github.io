4a4640ffe15ed16b1d9edf539e891ad23da3d69d
Ryan Shee <ryan.shee@gmail.com>
2019-06-24 00:14:09 -0700

    FE3H page and slideshow style changes

---
+++ scripts/slideshow.js
@@ -0,0 +1,23 @@
+let imgArray = [
+];
+
+let filepath = '/static/img/fe3h/';
+for (let i = 1; i < 27; i++) {
+	imgArray.push(filepath + i.toString().padStart(2, '0') + ".jpg");
+}
+
+curIndex = -1;
+imgDuration = 5000;
+
+function slideShow() {
+	document.getElementById('slider').className += "fadeOut";
+	setTimeout(function() {
+		document.getElementById('slider').src = imgArray[curIndex];
+		document.getElementById('slider').className = "";
+	},750);
+	curIndex++;
+	if (curIndex == imgArray.length) { curIndex = 0; }
+	setTimeout(slideShow, imgDuration);
+}
+
+slideShow();
