0738e864bef80f5badaaf7af56943f708dc22cdd
Ryan Shee <ryan.shee@gmail.com>
2021-04-12 01:42:20 -0700

    Cleanup and make dropdown use click

--- scripts/imissmycafe/main.js
+++ scripts/imissmycafe/main.js
@@ -38,19 +38,149 @@ playlistMap.set('Anime', animePlaylists);
 playlistMap.set('Games', gamesPlaylists);
 
 var playlistHolder = "Lo-fi";
-var currentPlaylist = "Lo-fi";
+var currentPlaylist = playlistHolder;
 var currentIndex = 1;
 var currentBackground = "Rain";
 
 window.onload = function() {
-	
+	registerEvents();
+	manageURLArgs();
+};
 
+function playlistSelected(playlistName) {
+	// Close dropdown.
+	dropdownLeave(null, 'playlist');
+
+	// Save playlist name temporarily.
+	playlistHolder = playlistName;
+
+	changeButtonText('playlist', playlistName);
+
+	manageSelectedOption('playlist', playlistName);
+
+	// Clear div of all child elements.
+	let playlistParent = document.getElementById('playlist-parent');
+	while (playlistParent.firstChild) {
+		playlistParent.removeChild(playlistParent.lastChild);
+	}
+
+	// Add playlists as children to div.
+	if (playlistName != "Use My Own") {
+		let playlists = playlistMap.get(playlistName);
+		for (let i = 1; i <= playlists.length; i++) {
+			let link = document.createElement('p');
+			link.id = i;
+			let num = document.createTextNode(padWithZeroes(i, 2));
+			if (link.addEventListener) {
+				link.addEventListener('click', e => subPlaylistSelected(e, i), false);
+			} else {
+				link.attachEvent('onclick',  e => subPlaylistSelected(e, i));
+			}
+			link.appendChild(num);
+			playlistParent.appendChild(link);
+		}
+
+		if (playlistName == currentPlaylist) {
+			uncolorSubPlaylists(currentIndex);
+		}
+	} else {
+		console.log('drop textbox here');
+	}
+}
+
+function subPlaylistSelected(event, index) {
+	currentPlaylist = playlistHolder;
+	currentIndex = parseInt(index);
+	uncolorSubPlaylists(index);
+	buildURL();
+}
+
+function uncolorSubPlaylists(index) {
+	let nodes = document.getElementById('playlist-parent').childNodes;
+	nodes.forEach(node => {
+		node.style.color = 'white';
+		node.style.borderBottom = '0px';
+	});
+
+	let subPlaylist = document.getElementById(index);
+	subPlaylist.style.color = '#a7bfff';
+	subPlaylist.style.borderBottom = '2px solid #a7bfff';
+}
+
+function backgroundSelected(background) {
+	dropdownLeave(null, 'background');
+	currentBackground = background;
+	changeButtonText('background', background);
+	manageSelectedOption('background', background);
+	buildURL();
+}
+
+function changeButtonText(type, selection) {
+	let select = document.getElementById(type + '-select');
+	select.innerHTML = selection;
+}
+
+function submit(event) {
+	if (event.keyCode === 13) {
+		let input = document.getElementById("input");
+		refreshPlaylist(input.value);
+	}
+}
+
+function refreshPlaylist(playlistId) {
+	let iframe = document.getElementById("playlist");
+	iframe.src = "";
+	iframe.src = embedLink + playlistId;
+
+	let input = document.getElementById("input");
+	input.value = "";
+
+	localStorage.setItem("lastPlaylistId", playlistId);
+}
+
+// TODO: Utility methods, move to another JS file.
+function registerEvents() {
 	let playlistParent = document.getElementById('playlist-select');
 	playlistParent.addEventListener('mouseover', e => resizeDropdownWidth(e, 'playlist'));
+
+	let playlistArea = document.getElementById('playlist-area');
+	playlistArea.addEventListener('mouseleave', e => dropdownLeave(e, 'playlist'));
 
 	let backgroundParent = document.getElementById('background-select');
 	backgroundParent.addEventListener('mouseover', e => resizeDropdownWidth(e, 'background'));
 
+	let backgroundArea = document.getElementById('background-area');
+	backgroundArea.addEventListener('mouseleave', e => dropdownLeave(e, 'background'));
+}
+
+function dropdownLeave(event, type) {
+	if (type != undefined) {
+		let dropdown = document.getElementById(type + '-dropdown');
+		dropdown.style.display = "none";
+
+		let select = document.getElementById(type + '-select');
+		select.style.borderBottomLeftRadius = '6px';
+		select.style.borderBottomRightRadius = '6px';
+	}
+}
+
+function dropdownClicked(type) {
+	let dropdown = document.getElementById(type + '-dropdown');
+	dropdown.style.display = 'block';
+
+	let select = document.getElementById(type + '-select');
+	select.style.borderBottomLeftRadius = '0px';
+	select.style.borderBottomRightRadius = '0px';
+}
+
+function resizeDropdownWidth(event, element) {
+	let playlistSelect = document.getElementById(element + '-select');
+	let buttonSize = playlistSelect.offsetWidth;
+	let dropdownParent = document.getElementById(element + '-dropdown');
+	dropdownParent.style.width = (buttonSize - 4) + 'px'; // -4 because the dropdown's box shadow is 2 pixels on both sides
+}
+
+function manageURLArgs() {
 	let url = window.location.href;
 	if (url.includes('#')) {
 		url = url.substr(url.indexOf('#') + 1);
@@ -78,24 +208,15 @@ window.onload = function() {
 		playlistSelected('Lo-fi');
 		backgroundSelected('Rain');
 	}
-};
-
-function resizeDropdownWidth(event, element) {
-	let playlistSelect = document.getElementById(element + '-select');
-	let buttonSize = playlistSelect.offsetWidth;
-	let dropdownParent = document.getElementById(element + '-dropdown');
-	dropdownParent.style.width = (buttonSize - 4) + 'px'; // -4 because the dropdown's box shadow is 2 pixels on both sides
 }
 
-function playlistSelected(playlistName) {
-	playlistHolder = playlistName;
+function manageSelectedOption(type, option) {
+	// Change button text.
+	let select = document.getElementById(type + '-select');
+	select.innerHTML = option;
 
-	// Set currently selected playlist name in the dropdown.
-	let select = document.getElementById('playlist-select');
-	select.innerHTML = playlistName;
-
-	// Clear styling of all options.
-	let nodes = document.getElementById('playlist-dropdown').childNodes;
+	// Set currently selected background name in the dropdown.
+	let nodes = document.getElementById(type + '-dropdown').childNodes;
 	nodes.forEach(node => {
 		if (node.style != undefined) {
 			node.innerHTML = node.innerHTML.substr(node.innerHTML.indexOf('✓ ') + 1);
@@ -103,74 +224,10 @@ function playlistSelected(playlistName) {
 		}
 	});
 
-	// Set style of selected playlist.
-	let selection = document.getElementById(playlistName);
+	// Set style of selected background.
+	let selection = document.getElementById(option);
 	selection.innerHTML = '✓ ' + selection.innerHTML;
 	selection.style.fontWeight = "bold";
-
-	// Clear div of all child elements.
-	let playlistParent = document.getElementById('playlist-parent');
-	while (playlistParent.firstChild) {
-		playlistParent.removeChild(playlistParent.lastChild);
-	}
-
-	// Add playlists as children to div.
-	if (playlistName != "Use My Own") {
-		let playlists = playlistMap.get(playlistName);
-		for (let i = 1; i <= playlists.length; i++) {
-			let link = document.createElement('p');
-			link.id = i;
-			let num = document.createTextNode(padWithZeroes(i, 2));
-			if (link.addEventListener) {
-				link.addEventListener('click', e => subPlaylistSelected(e, i), false);
-			} else {
-				link.attachEvent('onclick',  e => subPlaylistSelected(e, i));
-			}
-			link.appendChild(num);
-			playlistParent.appendChild(link);
-		}
-
-		if (playlistName == currentPlaylist) {
-			uncolorSubplaylists(currentIndex);
-		}
-	} else {
-		console.log('drop textbox here');
-	}
-}
-
-function padWithZeroes(number, length) {
-	let paddedString = '' + number;
-	while (paddedString.length < length) {
-		paddedString = '0' + paddedString;
-	}
-
-	return paddedString;
-}
-
-function subPlaylistSelected(event, index) {
-	currentPlaylist = playlistHolder;
-	currentIndex = parseInt(index);
-	uncolorSubplaylists(index);
-	buildURL();
-}
-
-function uncolorSubplaylists(index) {
-	let nodes = document.getElementById('playlist-parent').childNodes;
-	nodes.forEach(node => {
-		node.style.color = 'white';
-		node.style.borderBottom = '0px';
-	});
-
-	let subPlaylist = document.getElementById(index);
-	subPlaylist.style.color = '#a7bfff';
-	subPlaylist.style.borderBottom = '2px solid #a7bfff';
-}
-
-function backgroundSelected(background) {
-	currentBackground = background;
-	let select = document.getElementById('background-select');
-	select.innerHTML = background;
-	buildURL();
 }
 
 function buildURL() {
@@ -185,20 +242,11 @@ function buildURL() {
 	}
 }
 
-function submit(event) {
-	if (event.keyCode === 13) {
-		let input = document.getElementById("input");
-		refreshPlaylist(input.value);
+function padWithZeroes(number, length) {
+	let paddedString = '' + number;
+	while (paddedString.length < length) {
+		paddedString = '0' + paddedString;
 	}
-}
 
-function refreshPlaylist(playlistId) {
-	let iframe = document.getElementById("playlist");
-	iframe.src = "";
-	iframe.src = embedLink + playlistId;
-
-	let input = document.getElementById("input");
-	input.value = "";
-
-	localStorage.setItem("lastPlaylistId", playlistId);
+	return paddedString;
 }
